c83ffa6d1f8f11e29f4ac7c44c15bb0068dd2ddb
VictorOdoyo <victorodoyo555@gmail.com>
2024-02-13 14:35:46 +0300

    lyft Task

--- car.py
+++ car.py
@@ -8,3 +8,14 @@ class Car(ABC):
     @abstractmethod
     def needs_service(self):
         pass
+
+from serviceable import Serviceable
+
+
+class Car(Serviceable):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
+
+    def needs_service(self):
+        return self.engine.needs_service() or self.battery.needs_service()
